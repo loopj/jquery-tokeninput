f1730d1bfdae01d1de96c539ebfb7cd7bb97d4ab
Mark Carver <mark.carver@me.com>
2010-10-03 22:55:58 -0800

    Added prepopulate.js and tvshows.js

---
+++ examples/tvshows.js
@@ -0,0 +1,14 @@
+[
+  {"id":"978","name":"cha$e", "description": "2008", "category": "Reality TV" },
+  {"id":"1530","name":"The Life and Times of Tim", "description": "2008-", "category": "Comedy"},
+  {"id":"1210","name":"Kenny vs. Spenny", "description": "2008-", "category": "Comedy"},
+  {"id":"1393","name":"Sex and the City", "description": "1998-2004", "category": "Romance"},
+  {"id":"1394","name":"Shark", "description": "2006-2008", "category": "Drama"},
+  {"id":"1395","name":"Shaun the Sheep", "description": "2007-", "category": "Family"},
+  {"id":"1398","name":"Side Order of Life", "description": "2007", "category": "Drama"},
+  {"id":"1397","name":"Shear Genius", "description": "2007-", "category": "Reality TV"},
+  {"id":"1396","name":"Seinfeld", "description": "1990-1998", "category": "Comedy"},
+  {"id":"1399","name":"Sinchronicity", "description": "2006", "category": "Comedy"}
+]
+
+// [ { "name": "role number 1", "id": "rid:194", "description": "(rid: 194)", "category": "Roles" }, { "name": "another role is here", "id": "rid:195", "description": "(rid: 195)", "category": "Roles" }, { "name": "simple", "id": "rid:196", "description": "(rid: 196)", "category": "Roles" }, { "name": "test1", "id": "uid:5333", "description": "(uid: 5333)", "category": "Users" }, { "name": "user_number_3", "id": "uid:5334", "description": "(uid: 5334)", "category": "Users" }, { "name": "just.another.user", "id": "uid:5335", "description": "(uid: 5335)", "category": "Users" } ]
