28e2b520d6ecfce595147c48d1aff544fa68f02f
James Smith <james@loopj.com>
2011-03-13 03:45:16 -0700

    Crossdomain by default was a bad idea

--- src/jquery.tokeninput.js
+++ src/jquery.tokeninput.js
@@ -26,7 +26,7 @@ $.fn.tokenInput = function (url, options) {
         onResult: null,
         onAdd: null,
         onDelete: null,
-        crossDomain: true
+        crossDomain: false
     }, options);
 
     settings.classes = $.extend({
