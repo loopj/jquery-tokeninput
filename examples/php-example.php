82325bd92f9cedb4eadf1fd14efcb00be6801c1d
James Smith <james@loopj.com>
2011-03-13 18:48:55 -0700

    Update readme and php example

--- examples/php-example.php
+++ examples/php-example.php
@@ -1,15 +1,33 @@
 <?
-mysql_pconnect("localhost", "username", "password") or die("Could not connect");
-mysql_select_db("mydatabase") or die("Could not select database");
- 
-$query = sprintf("SELECT id, name from mytable WHERE name LIKE '%%%s%%' ORDER BY something DESC LIMIT 10", mysql_real_escape_string($_GET["q"]));
+
+#
+# Example PHP server-side script for generating
+# responses suitable for use with jquery-tokeninput
+#
+
+# Connect to the database
+mysql_pconnect("host", "username", "password") or die("Could not connect");
+mysql_select_db("database") or die("Could not select database");
+
+# Perform the query
+$query = sprintf("SELECT id, name from mytable WHERE name LIKE '%%%s%%' ORDER BY popularity DESC LIMIT 10", mysql_real_escape_string($_GET["q"]));
 $arr = array();
 $rs = mysql_query($query);
 
-while($obj = mysql_fetch_object($rs))
-{
+# Collect the results
+while($obj = mysql_fetch_object($rs)) {
     $arr[] = $obj;
 }
 
-echo json_encode($arr);
+# JSON-encode the response
+$json_response = json_encode($arr);
+
+# Optionally: Wrap the response in a callback function for JSONP cross-domain support
+if($_GET["callback"]) {
+    $json_response = $_GET["callback"] . "(" . $json_response . ")";
+}
+
+# Return the response
+echo $json_response;
+
 ?>
