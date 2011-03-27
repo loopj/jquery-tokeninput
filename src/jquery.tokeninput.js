7055a4de6e6e179c880770f83bc9bef226ddea52
James Smith <james@loopj.com>
2011-03-26 19:08:05 -0700

    Fix unicode support object

--- src/jquery.tokeninput.js
+++ src/jquery.tokeninput.js
@@ -54,38 +54,21 @@ var POSITION = {
 
 // Keys "enum"
 var KEY = {
-    ALT: 18
     BACKSPACE: 8
-    CAPS_LOCK: 20
+    TAB: 9,
+    ENTER: 13,
+    ESCAPE: 27,
+    SPACE: 32,
+    PAGE_UP: 33,
+    PAGE_DOWN: 34,
+    END: 35,
+    HOME: 36,
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+    NUMPAD_ENTER: 108,
     COMMA: 188
-    COMMAND: 91
-    COMMAND_LEFT: 91
-    COMMAND_RIGHT: 93
-    CONTROL: 17
-    DELETE: 46
-    DOWN: 40
-    END: 35
-    ENTER: 13
-    ESCAPE: 27
-    HOME: 36
-    INSERT: 45
-    LEFT: 37
-    MENU: 93
-    NUMPAD_ADD: 107
-    NUMPAD_DECIMAL: 110
-    NUMPAD_DIVIDE: 111
-    NUMPAD_ENTER: 108
-    NUMPAD_MULTIPLY: 106
-    NUMPAD_SUBTRACT: 109
-    PAGE_DOWN: 34
-    PAGE_UP: 33
-    PERIOD: 190
-    RIGHT: 39
-    SHIFT: 16
-    SPACE: 32
-    TAB: 9
-    UP: 38
-    WINDOWS: 91
 };
 
 
@@ -220,6 +203,7 @@ $.TokenList = function (input, settings) {
 
                 case KEY.TAB:
                 case KEY.ENTER:
+                case KEY.NUMPAD_ENTER:
                 case KEY.COMMA:
                   if(selected_dropdown_item) {
                     add_token($(selected_dropdown_item));
