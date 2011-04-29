e670df6cf009609c12764f0837def563190406b5
Gregor Martynus <gregor@martynus.net>
2011-04-29 07:12:08 -0700

    You'll get errors if highlight_term is run with a term containing RegExp special chars. So you better escape'em.
    Cheers

--- src/jquery.tokeninput.js
+++ src/jquery.tokeninput.js
@@ -546,7 +546,8 @@ $.TokenList = function (input, url_or_data, settings) {
 
     // Highlight the query part of the search term
     function highlight_term(value, term) {
-        return value.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + term + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<b>$1</b>");
+        var escaped = term.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+        return value.replace(new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + escaped + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<b>$1</b>");
     }
 
     // Populate the results dropdown with some results
