8da9413024857dcfefcb68a28b6f74a050318376
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2011-06-06 11:43:03 -0700

    Dropdown takes the list's width
    Cherry-picked from https://github.com/ignacioola/jquery-tokeninput/commit/7f309beae1d3f051b4b778a763741087c4832630

--- src/jquery.tokeninput.js
+++ src/jquery.tokeninput.js
@@ -668,7 +668,8 @@ $.TokenList = function (input, url_or_data, settings) {
                 position: "absolute",
                 top: $(token_list).offset().top + $(token_list).outerHeight(),
                 left: $(token_list).offset().left,
-                zindex: 999
+                zindex: 999,
+                width: $(token_list).width()
             })
             .show();
     }
