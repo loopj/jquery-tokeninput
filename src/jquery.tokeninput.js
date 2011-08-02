5f524d2136ead2d6c8353edf439fa163f8c1eddd
Jan Pieper <kontakt@jan-pieper.info>
2011-08-02 21:40:10 +0200

    removed parameter from onReady-callback

--- src/jquery.tokeninput.js
+++ src/jquery.tokeninput.js
@@ -361,7 +361,7 @@ $.TokenList = function (input, url_or_data, settings) {
 
     // Initialization is done
     if($.isFunction(settings.onReady)) {
-        settings.onReady.call(this);
+        settings.onReady.call();
     }
 
     //
