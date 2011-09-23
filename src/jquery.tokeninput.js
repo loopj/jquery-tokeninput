6dfc5ddb944d32ba21ee1ffca43b176b0da5ed0d
Ben Morris <github@benmorris.net>
2011-09-22 20:28:42 -0400

    Added emptyText parameter to settings for default search field display contents

--- src/jquery.tokeninput.js
+++ src/jquery.tokeninput.js
@@ -1,7 +1,7 @@
 /*
  * jQuery Plugin: Tokenizing Autocomplete Text Entry
  * Version 1.6.0
- *
+ * 
  * Copyright (c) 2009 James Smith (http://loopj.com)
  * Licensed jointly under the GPL and MIT licenses,
  * choose which one suits your project best!
