971da2d8bd9ae62c916d19193463c63237443041
kyle shay <kyle.shay@ymail.com>
2012-06-22 16:51:35 -0300

    maybe i'm misunderstanding the idea for the tokenValue... in my data I need the value of the item to be 'key' not 'id'... it would be fine, but if you look at the lines i changed... i can now over ride the value to set it to be 'key'

--- src/jquery.tokeninput.js
+++ src/jquery.tokeninput.js
@@ -540,7 +540,7 @@ $.TokenList = function (input, url_or_data, settings) {
             token_list.children().each(function () {
                 var existing_token = $(this);
                 var existing_data = $.data(existing_token.get(0), "tokeninput");
-                if(existing_data && existing_data.id === item.id) {
+                if(existing_data && existing_data[settings.tokenValue] === item[settings.tokenValue]) {
                     found_existing_token = existing_token;
                     return false;
                 }
