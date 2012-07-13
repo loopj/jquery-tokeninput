5871fcdb85eaa72ae337d1d1a6051eb2da252937
Raihan Mahmud Arman <raihan2006i@gmail.com>
2012-07-13 13:09:10 +0600

    Bug fix : Uncaught TypeError: Object [object Object] has no method 'prop'

--- src/jquery.tokeninput.js
+++ src/jquery.tokeninput.js
@@ -457,13 +457,13 @@ $.TokenList = function (input, url_or_data, settings) {
         } else {
             $(input).data("settings").disabled = !$(input).data("settings").disabled;
         }
-        input_box.prop('disabled', $(input).data("settings").disabled);
+        input_box.attr('disabled', $(input).data("settings").disabled);
         token_list.toggleClass($(input).data("settings").classes.disabled, $(input).data("settings").disabled);
         // if there is any token selected we deselect it
         if(selected_token) {
             deselect_token($(selected_token), POSITION.END);
         }
-        hidden_input.prop('disabled', $(input).data("settings").disabled);
+        hidden_input.attr('disabled', $(input).data("settings").disabled);
     }
 
     function checkTokenLimit() {
