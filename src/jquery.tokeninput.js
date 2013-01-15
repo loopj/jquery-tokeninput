de72882f309bc8b097e2eb3eba154e1cc98f7aa2
jnfsmile <jnfsmile@gmail.com>
2013-01-15 11:34:30 +0200

    RTL functionality (Right/Left keys)

--- src/jquery.tokeninput.js
+++ src/jquery.tokeninput.js
@@ -34,9 +34,8 @@ var DEFAULT_SETTINGS = {
     theme: null,
     zindex: 999,
     resultsLimit: null,
+    rtl:false,
 
-    //Behavioral settings
-    immediateDeletion:false,
     enableHTML: false,
 
     resultsFormatter: function(item) {
@@ -58,6 +57,7 @@ var DEFAULT_SETTINGS = {
     // Behavioral settings
     allowFreeTagging: false,
     allowTabOut: false,
+    immediateDeletion: false,
 
     // Callbacks
     onResult: null,
@@ -279,22 +279,22 @@ $.TokenList = function (input, url_or_data, settings) {
 
                         if((previous_token.length && previous_token.get(0) === selected_token) || (next_token.length && next_token.get(0) === selected_token)) {
                             // Check if there is a previous/next token and it is selected
-                            if(event.keyCode === KEY.LEFT || event.keyCode === KEY.UP) {
+                            if (event.keyCode === ($(input).data("settings").rtl ? KEY.RIGHT : KEY.LEFT) || event.keyCode === KEY.UP) {
                                 deselect_token($(selected_token), POSITION.BEFORE);
                             } else {
                                 deselect_token($(selected_token), POSITION.AFTER);
                             }
-                        } else if((event.keyCode === KEY.LEFT || event.keyCode === KEY.UP) && previous_token.length) {
+                        } else if ((event.keyCode === ($(input).data("settings").rtl ? KEY.RIGHT : KEY.LEFT) || event.keyCode === KEY.UP) && previous_token.length) {
                             // We are moving left, select the previous token if it exists
                             select_token($(previous_token.get(0)));
-                        } else if((event.keyCode === KEY.RIGHT || event.keyCode === KEY.DOWN) && next_token.length) {
+                        } else if ((event.keyCode === ($(input).data("settings").rtl ? KEY.LEFT : KEY.RIGHT) || event.keyCode === KEY.DOWN) && next_token.length) {
                             // We are moving right, select the next token if it exists
                             select_token($(next_token.get(0)));
                         }
                     } else {
                         var dropdown_item = null;
 
-                        if(event.keyCode === KEY.DOWN || event.keyCode === KEY.RIGHT) {
+                        if (event.keyCode === KEY.DOWN || event.keyCode === ($(input).data("settings").rtl ? KEY.LEFT : KEY.RIGHT)) {
                             dropdown_item = $(selected_dropdown_item).next();
                         } else {
                             dropdown_item = $(selected_dropdown_item).prev();
