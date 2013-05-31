b63e5aa50283de51ff48be22140c7672f09534e8
Christopher Thielen <cthielen@gmail.com>
2013-05-31 13:43:06 -0700

    Add support for custom CSS class tokens via item.class attribute

--- src/jquery.tokeninput.js
+++ src/jquery.tokeninput.js
@@ -598,6 +598,7 @@ $.TokenList = function (input, url_or_data, settings) {
         var readonly = item.readonly === true ? true : false;
 
         if(readonly) $this_token.addClass($(input).data("settings").classes.tokenReadOnly);
+        if(item.class) $this_token.addClass(item.class);
 
         $this_token.addClass($(input).data("settings").classes.token).insertBefore(input_token);
 
