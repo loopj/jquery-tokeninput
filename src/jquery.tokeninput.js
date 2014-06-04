85b1b1a7bae3c3b8165df05dc36bac5c993d4508
JonMidhir <info@midhirrecords.com>
2014-06-04 11:00:45 +0100

    Respect placeholder attr on original input, unless overridden in settings.

--- src/jquery.tokeninput.js
+++ src/jquery.tokeninput.js
@@ -382,6 +382,11 @@
               }
           });
 
+      // Copy original placeholder unless overridden
+      if (!settings.placeholder) {
+        settings.placeholder = $(input).attr('placeholder')
+      }
+
       // Keep reference for placeholder
       if (settings.placeholder) {
         input_box.attr("placeholder", settings.placeholder);
