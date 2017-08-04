3ccbb27d6aa0ff6130b0356edf252773a4ac90e2
Antonio Fortunato <fortuu@gmail.com>
2017-08-04 16:27:32 +0200

    minChars: 0 will show all results
    Bug fix.
    Now when  minChars: 0 will show the full list of results

--- src/jquery.tokeninput.js
+++ src/jquery.tokeninput.js
@@ -258,7 +258,16 @@
                   return false;
               } else
               if ($(input).data("settings").tokenLimit === null || $(input).data("settings").tokenLimit !== token_count) {
-                  show_dropdown_hint();
+                  if ($(input).data("settings").minChars == 0) {
+
+                        // set a timeout just long enough to let this function finish.
+                        setTimeout(function () {
+                            do_search();
+                        }, 5);
+                    } else {
+
+                        show_dropdown_hint();
+                    }
               }
               token_list.addClass($(input).data("settings").classes.focused);
           })
@@ -950,7 +959,7 @@
       function do_search() {
           var query = input_box.val();
 
-          if(query && query.length) {
+          if ((query && query.length) || $(input).data("settings").minChars == 0) {
               if(selected_token) {
                   deselect_token($(selected_token), POSITION.AFTER);
               }
